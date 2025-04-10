2db5e89869ce70bf8242dc6d36ed35e07ba315d9
golds1234 <58625309+golds1234@users.noreply.github.com>
2025-04-11 03:08:56 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd2f3e4a5-bb3c-4e2e-8c57-9a5b7f0d3e6a';
+let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
 const พร็อกซีไอพีs = ['www.samsung.com', 'www.adobe.com'];
 
